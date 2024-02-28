c02577fe5bc024a1641cfd106830dff4cbd1e981
Rongmario <rongmario@protonmail.com>
2024-02-28 01:04:11 +0000

    Sampling stages

---
+++ src/main/java/com/cleanroommc/flare/common/sampler/SamplingStage.java
@@ -0,0 +1,18 @@
+package com.cleanroommc.flare.common.sampler;
+
+public enum SamplingStage {
+
+    GAME_LOAD,
+    WORLD_LOAD,
+    CORE_MOD,
+    CONSTRUCTION,
+    PRE_INIT,
+    INIT,
+    POST_INIT,
+    AVAILABLE,
+    FINALIZING,
+    BEFORE_START_WORLD,
+    STARTING_WORLD,
+    STARTED_WORLD;
+
+}
