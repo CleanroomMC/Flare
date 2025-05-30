65abb0ad011fe0c495bf2b7b4613b2975aab881b
Rongmario <rongmario@proton.me>
2025-05-30 14:48:51 +0100

    Rremove redundant copies + remove unused map + make messages clearer

--- src/main/java/com/cleanroommc/flare/util/ChatUtil.java
+++ src/main/java/com/cleanroommc/flare/util/ChatUtil.java
@@ -1,7 +1,6 @@
 package com.cleanroommc.flare.util;
 
 import com.cleanroommc.flare.api.FlareAPI;
-import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TextComponentString;
@@ -9,34 +8,15 @@ import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.util.text.TextFormatting;
 
 import java.text.DecimalFormat;
-import java.util.Map;
 import java.util.function.Consumer;
 
 public final class ChatUtil {
 
-    public static final String RAW_PREFIX = "[⚡] ";
-    public static final ITextComponent RESET = new TextComponentString("");
-    public static final ITextComponent PREFIX = new TextComponentString("[");
+    public static final String RAW_PREFIX = TextFormatting.GRAY + "[" + TextFormatting.GOLD + "✳" + TextFormatting.GRAY + "]" + TextFormatting.RESET + " ";
     public static final DecimalFormat FLOAT_FORMAT = new DecimalFormat("#.##");
 
-    private static final Map<String, LangKeys> langKeys = new Object2ObjectOpenHashMap<>();
-
-    static {
-        RESET.getStyle().setColor(TextFormatting.RESET);
-        PREFIX.getStyle().setColor(TextFormatting.DARK_GRAY);
-        ITextComponent symbol = new TextComponentString("⚡");
-        symbol.getStyle().setColor(TextFormatting.YELLOW);
-        PREFIX.appendSibling(symbol);
-        ITextComponent closingBracket = new TextComponentString("] ");
-        closingBracket.getStyle().setColor(TextFormatting.DARK_GRAY);
-        PREFIX.appendSibling(closingBracket);
-        for (LangKeys langKey : LangKeys.values()) {
-            langKeys.put(langKey.langKey, langKey);
-        }
-    }
-
     public static ITextComponent prefix(ITextComponent message) {
-        return PREFIX.createCopy().appendSibling(RESET.createCopy()).appendSibling(message.createCopy());
+        return new TextComponentString(RAW_PREFIX).appendSibling(message);
     }
 
     public static void sendMessage(FlareAPI flare, ICommandSender sender, ITextComponent textComponent) {
